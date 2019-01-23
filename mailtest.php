d5fea54d2f3c4746d7715c843b2e462a3d82efdc
Glenn451 <glenn451@gmail.com>
2019-01-23 00:33:48 -0800

    2nd mail fix
    2nd mail fix, removed original mail code

--- mailtest.php
+++ mailtest.php
@@ -1,3 +1,19 @@
 <?php
-mail('alouie987@yahoo.com','test email','it worked!','From: noreply@mygifter.com');
+//mail variable setup
+$to = 'glenn451@gmail.com';
+$subject = 'test mail';
+
+//message content
+$message = 'this is a test';
+
+// word wrap after 70 chars
+$message = wordwrap($message, 70);
+
+//header field
+$headers = 'From: webmaster@mygifter.com' . "\r\n" .
+    'Reply-To: alouie987@yahoo.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+//the actual mail command
+mail($to, $subject, $message, $headers);
 ?>
